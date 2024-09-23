7e853a52d5b4c21e240150298d889f4f42a461db
gabriel.holz <gabrielholzdecastro@outlook.com>
2024-09-23 18:50:12 -0300

    first commit

---
+++ src/test/java/br/com/gabrielholzdecastro/school_service/SchoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielholzdecastro.school_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
